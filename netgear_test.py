7685e0e4d1d3191ae023a88b4c20b425a03f8be7
ethan <aikszhang@gmail.com>
2019-02-03 16:56:57 +1100

    Add speed monitor functions for the latest 10 sec

--- netgear_test.py
+++ netgear_test.py
@@ -1,9 +1,57 @@
 from pynetgear import Netgear
+import json
+import time
+import queue
 
-netgear = Netgear(password='enlightenedBAF7890')
+PASSWORD = ''
+NEW_TODAY_UPLOAD = 'NewTodayUpload'
+NEW_TODAY_DOWNLOAD = 'NewTodayDownload'
+
+def get_traffic_json(netgear):
+    traffic = netgear.get_traffic_meter()
+    traffic_json = json.dumps(traffic)
+    traffic_json = json.loads(traffic_json)
+    return traffic_json
+
+def get_today_upload_and_download(netgear):
+    traffic_json = get_traffic_json(netgear)
+    new_today_upload = traffic_json[NEW_TODAY_UPLOAD]
+    new_today_download = traffic_json[NEW_TODAY_DOWNLOAD]
+    return new_today_upload, new_today_download
+
+def main():
+    netgear = Netgear(password=PASSWORD)
+    upload_queue = queue.Queue()
+    download_queue = queue.Queue()
+
+    cumulative_upload = 0
+    cumulative_download = 0
+    duration = 1
+    init_upload, init_download = get_today_upload_and_download(netgear)
+    upload_queue.put(init_upload)
+    download_queue.put(init_download)
+
+    while True:
+        time.sleep(1)
+        new_upload, new_download = get_today_upload_and_download(netgear)
+        upload_queue.put(new_upload)
+        download_queue.put(new_download)
+        if duration < 10:
+            duration += 1
+        else:
+            init_upload = upload_queue.get()
+            init_download = download_queue.get()
+
+        upload_rate = (new_upload - init_upload) / duration
+        download_rate = (new_download - init_download) / duration
+
+        print("Upload Rate: ", upload_rate * 1024,"KB,\tDownload Rate: ", download_rate, "MB")
+
 
 #for i in netgear.get_attached_devices():
 #    print(i)
 
-traffic = netgear.get_traffic_meter()
-print(traffic)
+
+if __name__ == "__main__":
+    main()
+
