92ea6b5dcb2bdb364b2016caf1a6104199e2a412
ethan <aikszhang@gmail.com>
2019-02-07 22:45:30 +1100

    Check device online status

--- netgear_get_devices.py
+++ netgear_get_devices.py
@@ -4,7 +4,7 @@ import time
 import queue
 import sys
 
-PASSWORD = ''
+ROUTER_ADMIN_PASSWORD = 'router_admin_password'
 DEVICES = 'devices'
 LABEL = 'label'
 NAME = 'name'
@@ -17,10 +17,18 @@ class Device:
         self.name = name
         self.mac = mac
         self.is_attached = is_attached
+    def __eq__(self, other):
+        if isinstance(other, Device):
+            return self.mac == other.mac
+        return false
     def __str__(self):
         return self.label + ", " + self.name + ", " + self.mac + ", " + str(self.is_attached)
 
 
+def get_router_admin_password():
+    with open('config.json') as config_json:
+        config = json.load(config_json)
+        return config[ROUTER_ADMIN_PASSWORD]
 
 def get_config_devices():
     config_devices = []
@@ -40,7 +48,7 @@ def get_attached_devices(netgear):
     # Device(signal=100, ip='10.0.0.5', name='eth4mbp', mac='DC:A9:04:75:34:ED', type='wireless', link_rate=263, allow_or_block='Allow', device_type=None, device_model=None, ssid=None, conn_ap_mac=None)
     attached_devices = []
     for device in netgear.get_attached_devices():
-        attached_devices.append((device[2], device[3]))
+        attached_devices.append(Device(mac = device[3], name = device[2], is_attached = True))
 
     if len(attached_devices) == 0:
         print("No devices online")
@@ -50,13 +58,23 @@ def get_attached_devices(netgear):
 
 def check(config_devices, attached_devices):
     for device in config_devices:
-        print(device)
-    print(attached_devices)
+        if device in attached_devices:
+            device.is_attached = True
+    return config_devices
+
+
 
 
 def main():
-    netgear = Netgear(password=PASSWORD)
-    check(get_config_devices(), get_attached_devices(netgear))
+    netgear = Netgear(password=get_router_admin_password())
+    config_devices = check(get_config_devices(), get_attached_devices(netgear))
+
+    for device in config_devices:
+        if device.is_attached:
+            print(device.label + ": Online" )
+        else:
+            print(device.label + ": Offline" )
+
 
 if __name__ == "__main__":
     main()
